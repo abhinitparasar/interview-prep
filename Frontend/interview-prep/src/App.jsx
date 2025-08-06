8fa5737035044c8e1aa3ffcb714c3c52435af80a
Abhinit Parasar <parasarabhinit7@gmail.com>
2025-08-06 22:02:46 +0530

    feat(frontend): setup basic react app with vite

---
+++ Frontend/interview-prep/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div >
+        <h1>Hello From the IntelliPrep Ai</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, ex, amet beatae quam provident temporibus corrupti tempore cum harum eum vero. Veniam temporibus veritatis quaerat atque ullam, consequuntur hic accusamus.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
